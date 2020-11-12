762681660268fa2d60ab018459b38e05c71f16b4
tsadimas@hua.gr <tsadimas@hua.gr>
2020-11-12 14:26:55 +0200

    added form and proccessform urls and jsps

--- src/gr/hua/dit/ds/springmvcdemo1/HomeController.java
+++ src/gr/hua/dit/ds/springmvcdemo1/HomeController.java
@@ -1,7 +1,11 @@
 package gr.hua.dit.ds.springmvcdemo1;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
@@ -10,4 +14,24 @@ public class HomeController {
 	public String hello() {
 		return "hello";
 	}
+	
+	@RequestMapping("/studentform")
+	public String showForm() {
+		return "student-form";
+	}
+	
+	@RequestMapping(value="/processform", method = RequestMethod.POST)
+	public String processFormv2(HttpServletRequest request, Model model) {
+
+	    String theName = request.getParameter("studentName");
+	    theName = theName.toUpperCase();
+
+	    String result ="Hello " + theName;
+
+	    model.addAttribute("message",result);
+
+	    return "student";
+	}
+
+
 }
