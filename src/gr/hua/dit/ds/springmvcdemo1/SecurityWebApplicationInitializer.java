8f5e7763301714b38fc4da87a7e31869564ca943
tsadimas@hua.gr <tsadimas@hua.gr>
2020-12-02 10:57:36 +0200

    started spring security

---
+++ src/gr/hua/dit/ds/springmvcdemo1/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package gr.hua.dit.ds.springmvcdemo1;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
