27300faef7408cfbd44356be8912fdd6768955d5
tsadimas@hua.gr <tsadimas@hua.gr>
2020-12-03 14:46:57 +0200

    added login and logout buttons

--- src/gr/hua/dit/ds/springmvcdemo1/config/AppSecurityConfig.java
+++ src/gr/hua/dit/ds/springmvcdemo1/config/AppSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,6 +26,18 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
 				.usersByUsernameQuery("select username,password, enabled from user where username=?")
 				.authoritiesByUsernameQuery("select username, authority from authorities where username=?");
 
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests() // authorize
+		.antMatchers("/").permitAll() // allow "/" to be seen without authentication
+		.anyRequest().authenticated() // all requests are authenticated
+		.and()
+		.formLogin().permitAll() //allow "/login"
+		.and()
+		.logout().permitAll(); // allow "logout"
 	}
 
 	@Bean
